caf5ad31dd6865057ba894d0bb972737312ed2e8
Raju-4576 <rajubambhaniya.vision@gmail.com>
2025-04-01 18:32:16 +0530

    mychanes

---
+++ passport.js
@@ -0,0 +1,2 @@
+const google=require('passport-google-oauth20').Strategy;
+const passport=require(passport)
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const passport = require("passport");
+require('dotenv').config();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/login/success", (req, res) => {
+  if (req.user) {
+    res.status(200).json({
+      error: false,
+      message: "successfully log in",
+      user: req.user,
+    });
+  } else {
+    res.status(403).json({ messge: "not Authorised" });
+  }
 });
 
+router.get("/login/failed", (req, res) => {
+  res.status(401).json({
+    error: true,
+    message: "log in failed",
+  });
+});
+
+router.get("/google",passport.authenticate('google',{scope:['email','profile']}))
+
+router.get('/google/callback',passport.authenticate("google",{
+  successRedirect:process.env.CLIENT_URL
+}))
+
+router.get('/logout',(req,res)=>{
+  req.logout();
+  req.redirect(process.env.CLIENT_URL)
+})
 module.exports = router;
